f42fb5ec65fa101c35546fb38a4b87f3a7d65148
ilya <smakerok@gmail.com>
2022-11-14 00:47:23 +0300

    test updates

--- spec/api/test.user.spec.js
+++ spec/api/test.user.spec.js
@@ -18,7 +18,9 @@ describe('Get user info by id', () => {
       const response = await request.get(`${app.methods.api.testUser}${id}`);
       expect(response).statusCode(200);
       expect(response.body).toMatchSchema(app.schema.testUser);
+      expect(response.body.isSuccess).toBeTruthy();
       expect(response.body.user.gender).toEqual('male');
+      expect(response.body.user.id).toEqual(id);
       expect(parseInt(response.body.user.age)).toBeLessThan(80);
       expect(Date.parse(response.body.user.registrationDate)).toBeGreaterThan(Date.parse('2010-01-01'));
     }
@@ -30,7 +32,9 @@ describe('Get user info by id', () => {
       const response = await request.get(`${app.methods.api.testUser}${id}`);
       expect(response).statusCode(200);
       expect(response.body).toMatchSchema(app.schema.testUser);
+      expect(response.body.isSuccess).toBeTruthy();
       expect(response.body.user.gender).toEqual('female');
+      expect(response.body.user.id).toEqual(id);
       expect(parseInt(response.body.user.age)).toBeLessThan(80);
       expect(Date.parse(response.body.user.registrationDate)).toBeGreaterThan(Date.parse('2010-01-01'));
     }
@@ -44,6 +48,20 @@ describe('Get user info by id', () => {
       expect(response).statusCode(200);
       expect(response.body).toMatchSchema(app.schema.testUser);
     }
+  });
+
+  it(`Get user with incorrect number`, async () => {
+    const response = await request.get(`${app.methods.api.testUser}1337`);
+    expect(response).statusCode(400);
+    expect(response.body).toMatchSchema(app.schema.testUser);
+    console.log(response.body)
+  });
+
+  it(`Get user with incorrect string value`, async () => {
+    const response = await request.get(`${app.methods.api.testUser}iddqd`);
+    expect(response).statusCode(400);
+    expect(response.body).toMatchSchema(app.schema.testUser);
+    console.log(response.body)
   });
 
 });
--- spec/api/test.users.gender.spec.js
+++ spec/api/test.users.gender.spec.js
@@ -9,6 +9,7 @@ describe('Get userIds by gender', () => {
       const response = await request.get(`${app.methods.api.testUsersByGender}?gender=${gender.name}`);
       expect(response).statusCode(200);
       expect(response.body).toMatchSchema(app.schema.testUsersByGender);
+      expect(response.body.isSuccess).toBeTruthy();
       gender.ids = response.body.idList;
     });
   });
@@ -25,6 +26,20 @@ describe('Get userIds by gender', () => {
     const genderIds = [...maleIds, ...femaleIds];
     const containsAll = app.main.containsAll(genderIds, anyIds);
     expect(containsAll).toBeTruthy();
+  });
+
+  it(`Get userIds without gender`, async () => {
+    const response = await request.get(`${app.methods.api.testUsersByGender}`);
+    expect(response).statusCode(400);
+    expect(response.body).toMatchSchema(app.schema.testUsersByGender);
+    console.log(response.body)
+  });
+
+  it(`Get userIds with incorrect gender value`, async () => {
+    const response = await request.get(`${app.methods.api.testUsersByGender}?gender=magic`);
+    expect(response).statusCode(400);
+    expect(response.body).toMatchSchema(app.schema.testUsersByGender);
+    console.log(response.body)
   });
 
 });
